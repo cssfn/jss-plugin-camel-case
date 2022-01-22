ca8d5bb9b62f7df47c0ff8e76b19bd66966832a8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2022-01-23 03:03:08 +0700

    updated algorithm

--- dist/jss-plugin-camel-case.js
+++ dist/jss-plugin-camel-case.js
@@ -4,7 +4,7 @@ const isStyle = (object) => isLiteralObject(object);
 const renameProps = (style) => {
     // stores the style's entries __only_if__ the modification is needed:
     let styleArrLazy = null;
-    for (const [propName, propIndex] of Object.keys(style).map((propName, propIndex) => [propName, propIndex])) {
+    for (const [propName, propIndex] of Object.keys(style).map((propName, propIndex) => [propName, propIndex])) { // no need to iterate Symbol(s), because [prop: Symbol] is for storing nested rule
         if (propName.startsWith('--'))
             continue; // ignores css variable
         const newPropName = hyphenate(propName);
@@ -38,6 +38,8 @@ const renameProps = (style) => {
     return style; // no changes => return the original
 };
 const onProcessStyle = (style, rule, sheet) => {
+    if (!style)
+        return {};
     return renameProps(style);
 };
 const onChangeValue = (propValue, propName, rule) => {
